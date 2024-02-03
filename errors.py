9b47b1591af6424f83593016ac6ba146c0262b8e
Konomi <konomik14@gmail.com>
2024-02-03 17:34:21 +1100

    I love untracked files (oops)

---
+++ errors.py
@@ -0,0 +1,11 @@
+class UnmatchedKeycounts(Exception):
+    pass
+
+class SkinNotFound(Exception):
+    pass
+
+class MissingConfiguration(Exception):
+    pass
+
+class InvalidVersion(Exception):
+    pass
